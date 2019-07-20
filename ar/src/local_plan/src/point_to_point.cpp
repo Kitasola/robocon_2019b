533bec3d8e27ecd88b680deb7eac9773e8b16cb6
Kitasola <e1611@s.akashi.ac.jp>
2019-07-20 12:10:38 +0900

    パラメータはいまいちやけど動いた

--- ar/src/local_plan/src/point_to_point.cpp
+++ ar/src/local_plan/src/point_to_point.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/Twist.h>
 #include <queue>
 #include <ros/ros.h>
@@ -28,18 +29,21 @@ public:
     dummy_point.x = msgs.position.x;
     dummy_point.y = msgs.position.y;
     double x = msgs.orientation.x, w = msgs.orientation.w;
-    dummy_point.yaw = atan2(2 * x * w, x * x - w * w);
+    dummy_point.yaw = atan2(x * x - w * w, 2 * x * w);
     goal_point_list.push(dummy_point);
   }
-  void getCurrentPoint(const geometry_msgs::Pose &msgs) {
-    current_point.x = msgs.position.x;
-    current_point.y = msgs.position.y;
-    double x = msgs.orientation.x, w = msgs.orientation.w;
-    current_point.yaw = atan2(2 * x * w, x * x - w * w);
+  /* void getCurrentPoint(const geometry_msgs::Pose &msgs) { */
+  /* current_point.x = msgs.position.x; */
+  /* current_point.y = msgs.position.y; */
+  /* double x = msgs.orientation.x, w = msgs.orientation.w; */
+  /* current_point.yaw = atan2(2 * x * w, x * x - w * w); */
+  void getCurrentPoint(const geometry_msgs::Pose2D &msgs) {
+    current_point.x = msgs.x;
+    current_point.y = msgs.y;
+    current_point.yaw = msgs.theta;
   }
   void control() {
     ros::Time current = ros::Time::now();
-    prev = current;
     double duration = current.toSec() - start.toSec();
     double delta = current.toSec() - prev.toSec();
     bool is_accel_finish = true;
@@ -61,21 +65,32 @@ public:
     }
 
     // 目標地点に到達したかどうか
-    if (is_accel_finish && hypot(goal_point.x - current_point.x,
-                                 goal_point.y - current_point.y) < ERROR_MIN) {
-      if (!goal_point_list.empty()) {
-        goal_point = goal_point_list.front();
-        goal_point_list.pop();
-      } else {
-        for (int i = 0; i < 2; ++i) {
-          velocity_goal[i] = 0;
+    if (is_accel_finish) {
+      if (hypot(goal_point.x - current_point.x,
+                goal_point.y - current_point.y) < ERROR_MIN) {
+        if (!goal_point_list.empty()) {
+          goal_point = goal_point_list.front();
+          ROS_INFO_STREAM("Next Goal Point is " << goal_point.x << ", "
+                                                << goal_point.y << ", "
+                                                << goal_point.yaw * 180 / M_PI);
+          goal_point_list.pop();
+        } else {
+          for (int i = 0; i < 2; ++i) {
+            velocity_goal[i] = 0;
+          }
+          goal_twist.linear.x = velocity_goal[0];
+          goal_twist.linear.y = velocity_goal[1];
+          velocity_pub.publish(goal_twist);
+          return;
         }
       }
+      setParam();
     }
 
     goal_twist.linear.x = velocity_goal[0];
     goal_twist.linear.y = velocity_goal[1];
     velocity_pub.publish(goal_twist);
+    prev = current;
   }
 
 private:
@@ -88,19 +103,19 @@ private:
   int accel_polar[2] = {};
   double velocity_goal[2] = {}, velocity_max[2] = {}, velocity_min[2] = {},
          velocity_diff[2] = {};
-  double accel_time[2], const_time[2], decel_time[2];
-  constexpr static double VELOCITY_MIN = 10, VELOCITY_MAX = 1000,
-                          ACCEL_MAX = 100;
+  double accel_time[2] = {}, const_time[2] = {}, decel_time[2] = {};
+  constexpr static double VELOCITY_MIN = 500, VELOCITY_MAX = 1500,
+                          ACCEL_MAX = 2000;
   constexpr static double ERROR_MIN = 10;
   bool is_accel_finish[2] = {};
 
   void setParam() {
-    start = ros::Time::now();
 
     double length =
         hypot(goal_point.x - current_point.x, goal_point.y - current_point.y);
     double angle =
-        atan2(goal_point.x - current_point.x, goal_point.y - current_point.y);
+        atan2(goal_point.y - current_point.y, goal_point.x - current_point.x);
+    ROS_INFO_STREAM("Distance is " << length << ", " << angle * 180 / M_PI);
 
     double velocity =
         sqrt(2 * ACCEL_MAX * length / M_PI + pow(VELOCITY_MIN, 2));
@@ -113,6 +128,10 @@ private:
     velocity_min[1] = VELOCITY_MIN * sin(angle);
     for (int i = 0; i < 2; ++i) {
       velocity_goal[i] = velocity_min[i];
+    }
+    for (int i = 0; i < 2; ++i) {
+      ROS_INFO_STREAM("Velocity Parameter is " << i << ", " << velocity_min[i]
+                                               << ", " << velocity_max[i]);
     }
 
     double dummy_length[2] = {length * abs(cos(angle)),
@@ -138,7 +157,11 @@ private:
       }
       velocity_goal[i] = velocity_min[i];
       is_accel_finish[i] = false;
+      ROS_INFO_STREAM("Accel Time Parameter is " << i << ", " << accel_time[i]
+                                                 << ", " << const_time[i]
+                                                 << ", " << decel_time[i]);
     }
+    start = ros::Time::now();
   }
 };
 
@@ -146,12 +169,13 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "local_planner");
   ros::NodeHandle n;
 
-  PTP controller(&n, "wheel/velocity", "wheel/goal_point", "robot_pose");
+  PTP controller(&n, "wheel/velocity", "wheel/goal_point", "wheel/robot_pose");
 
-  constexpr double FREQ = 100;
+  constexpr double FREQ = 300;
   ros::Rate loop_rate(FREQ);
 
   while (ros::ok()) {
     ros::spinOnce();
+    controller.control();
   }
 }
