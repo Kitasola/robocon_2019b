23c3bc7a2221a362ed39d5aa2bf2a5380dfab054
Kitasola <e1611@s.akashi.ac.jp>
2019-09-09 16:56:07 +0900

    違う種類のサーボを作った

--- ar/mdd_slave/servo/main.cpp
+++ ar/mdd_slave/servo/main.cpp
@@ -6,7 +6,7 @@ ScrpSlave slave(PA_9, PA_10, PA_12, SERIAL_TX, SERIAL_RX, 0x0803e000);
 
 constexpr int NUM_PORT = 5;
 // 0: Motor, 1: Encoder, 2: Other
-constexpr int PORT_FUNCTION[NUM_PORT] = {0, 2, 2, 0, 0};
+constexpr int PORT_FUNCTION[NUM_PORT] = {0, 2, 2, 2, 2};
 
 constexpr int NUM_MOTOR_PORT = 4;
 constexpr int MAX_PWM = 250;
@@ -63,8 +63,10 @@ bool spinMotor(int cmd, int rx_data, int &tx_data) {
 }
 
 PwmOut towel[2] = {PwmOut(PA_3), PwmOut(PA_1)};
-constexpr float MIN_SERVO_PULSE = 780e-6;
-constexpr float MAX_SERVO_PULSE = 2250e-6;
+/* constexpr float MIN_SERVO_PULSE = 780e-6; */
+/* constexpr float MAX_SERVO_PULSE = 2250e-6; */
+constexpr float MIN_SERVO_PULSE = 0.5e-3;
+constexpr float MAX_SERVO_PULSE = 2.4e-3;
 float servoDegreeToPulse(int degree) {
   return map(degree, 0, 180, MIN_SERVO_PULSE, MAX_SERVO_PULSE) +
          MIN_SERVO_PULSE;
@@ -77,6 +79,12 @@ bool driveServo(int cmd, int rx_data, int &tx_data) {
     towel[0].pulsewidth(servoDegreeToPulse(rx_data));
     towel[1].pulsewidth(servoDegreeToPulse(180 - rx_data));
   }
+  return true;
+}
+
+AnalogIn r(PA_0);
+bool readRegister(int cmd, int rx_data, int &tx_data) {
+  tx_data = 255 * r.read();
   return true;
 }
 
@@ -95,13 +103,14 @@ int main() {
       break;
     }
   }
-  slave.addCMD(2, spinMotor);
+  /* slave.addCMD(2, spinMotor); */
   // slave.addCMD(3, spinMotor);
-  slave.addCMD(4, spinMotor);
-  slave.addCMD(5, spinMotor);
+  /* slave.addCMD(4, spinMotor); */
+  /* slave.addCMD(5, spinMotor); */
   slave.addCMD(11, driveServo);
   slave.addCMD(12, driveServo);
   slave.addCMD(13, driveServo);
+  slave.addCMD(14, readRegister);
   slave.addCMD(255, safe);
   while (true) {
   }
