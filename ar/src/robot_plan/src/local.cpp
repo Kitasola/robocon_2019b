b1cfcb325255f140221639c0a8554386411fe8da
Kitasola <e1611@s.akashi.ac.jp>
2019-09-11 09:29:56 +0900

    位置を変数に格納

--- ar/src/robot_plan/src/local.cpp
+++ ar/src/robot_plan/src/local.cpp
@@ -6,6 +6,13 @@
 #include <std_msgs/Bool.h>
 #include <vector>
 
+inline double pow2(double x) { return x * x; }
+
+struct AccelMap {
+  int position;
+  double velocity;
+}
+
 class SVelocity {
 
 public:
@@ -14,9 +21,9 @@ public:
         n->advertise<geometry_msgs::Twist>(username + "/velocity", 1);
     reach_goal_pub =
         n->advertise<geometry_msgs::Twist>(username + "/reach_goal", 1);
-    goal_point_sub = n->subscribe(username + "goal_point", 1,
+    goal_point_sub = n->subscribe(username + "/goal_point", 1,
                                   &SVelocity::getGoalPoint, this);
-    goal_velocity_sub = n->subscribe(username + "goal_velocity", 1,
+    goal_velocity_sub = n->subscribe(username + "/goal_velocity", 1,
                                      &SVelocity::getGoalVelocity, this);
     robot_pose_sub =
         n->subscribe("robot_pose", 1, &SVelocity::getRobotPose, this);
@@ -59,7 +66,7 @@ public:
 
     double dummy_velocity_max =
         ((VELOCITY_MIN + dummy_goal_velocity) / ACCEL_MAX +
-         sqrt(pow((VELOCITY_MIN + dummy_goal_velocity) / ACCEL_MAX, 2) +
+         sqrt(pow2((VELOCITY_MIN + dummy_goal_velocity) / ACCEL_MAX) +
               4 * dummy_distance / ACCEL_MAX)) /
         (4 / ACCEL_MAX);
     if (dummy_velocity_max > VELOCITY_MAX) {
@@ -69,12 +76,13 @@ public:
                               dummy_velocity_max * sin(angle)};
     double velocity_first[2] = {VELOCITY_MIN * cos(angle),
                                 VELOCITY_MIN * sin(angle)};
+    double velocity_final[2] = {goal_velocity.linear.x, goal_velocity.linear.y};
     for (int i = 0; i < 2; ++i) {
       if (velocity_first[i] < velocity_final_prev[i]) {
         velocity_first[i] = velocity_final_prev[i];
+        velocity_final_prev[i] = velocity_final[i];
       }
     }
-    double velocity_final[2] = {goal_velocity.linear.x, goal_velocity.linear.y};
     double accel_max[2] = {ACCEL_MAX * cos(angle), ACCEL_MAX * sin(angle)};
 
     double distance[2] = {dummy_distance * abs(cos(angle)),
@@ -83,37 +91,63 @@ public:
     int accel_polar[2] = {};
     for (int i = 0; i < 2; ++i) {
       accel_time[i] =
-          2 * abs(velocity_max[i] - velocity_first[i]) / accel_time[i];
+          2 * abs(velocity_max[i] - velocity_first[i]) / abs(accel_max[i]);
       const_time[i] =
           (distance[i] -
            2 * velocity_max[i] *
                (2 * velocity_max[i] - velocity_first[i] - velocity_final[i]) /
-               accel_max[i]) /
+               abs(accel_max[i])) /
               velocity_max[i] +
           accel_time[i];
       decel_time[i] =
           2 * abs(velocity_max[i] - velocity_final[i]) / accel_time[i] +
           const_time[i];
 
-      if (velocity_max[i] > velocity_first[i]) {
-        accel_polar[i] = 1;
-      } else {
-        accel_polar[i] = -1;
-      }
       ROS_INFO_STREAM("Velocity Parameter is " << i << ", " << velocity_first[i]
                                                << ", " << velocity_max[i]);
       ROS_INFO_STREAM("Accel Time Parameter is " << i << ", " << accel_time[i]
                                                  << ", " << const_time[i]
                                                  << ", " << decel_time[i]);
-    }
-    start_point = current_point;
-    double delta_t = 1.0 / rate_;
-    for (int time = 0; time * delta_t < decel_time[0]; ++time) {
-      if (time * delta_t < accel_time[0]) {
-      } else if (time * delta_t < const_time[0]) {
+      start_point = current_point;
+      double delta_t = 1.0 / rate_;
+      double dummy_start;
+      if (i == 0) {
+        dummy_start = start_point.x;
       } else {
+        dummy_start = start_point.y;
       }
-      /* velocity_map[0].push_back(); */
+      for (int j = 0; j * delta_t < decel_time[i]; ++j) {
+        double time = j * delta_t;
+        AccelMap data;
+        if (time < accel_time[i]) {
+          data.position =
+              accel_max[i] * pow2(accel_time[i]) / (8 * pow2(M_PI)) *
+                  (cos(2 * M_PI / accel_time[i] * time) - 1) +
+              accel_max[i] * pow2(time) / 2 + velocity_first[i] * time;
+          data.velocity = accel_max[i] * accel_time[i] / (4 * M_PI) *
+                              sin(2 * M_PI / accel_time[i] * time) +
+                          accel_max[i] * time / 2 + velocity_first[i];
+        } else if (time * delta_t < const_time[i]) {
+          data.position = velocity_max[i] * (time - accel_time[i]) +
+                          2 * velocity_max[i] *
+                              (velocity_max[i] - velocity_first[i]) /
+                              accel_max[i];
+          data.velocity = velocity_max[i];
+        } else {
+          data.position =
+              -accel_max[i] * pow2(accel_time[i]) / (8 * pow2(M_PI)) *
+                  (cos(2 * M_PI / accel_time[i] * (time - const_time[i]) - 1) -
+              accel_max[i] * pow2(time) / 2 + velocity_final[i] * time +
+              velocity_max[i] * (const_time[i] - accel_time[i]) +
+              2 * velocity_max[i] * (velocity_max[i] - velocity_first[i]) /
+                  accel_max[i];
+          data.velocity = -accel_max[i] * accel_time[i] / (4 * M_PI) *
+                              sin(2 * M_PI / accel_time[i] * (time - const_time[i])) +
+                          -accel_max[i] * time / 2 + velocity_final[i];
+        }
+        data.position += dummy_start;
+        velocity_map[0].push_back(data);
+      }
     }
   }
 
@@ -128,7 +162,7 @@ private:
   constexpr static double VELOCITY_MIN = 500, VELOCITY_MAX = 1000,
                           ACCEL_MAX = 1500;
   constexpr static double ERROR_DISTANCE_MAX = 10;
-  std::vector<double> velocity_map[2];
+  std::vector<AccelMap> velocity_map[2];
 
   arrc::PidVelocity moment{10, 0, 0};
 };
