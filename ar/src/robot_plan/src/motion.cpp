b8eaaa9ea5a5ca0566cbde2fd9c955a75c5fa5c9
Kitasola <e1611@s.akashi.ac.jp>
2019-09-14 04:21:40 +0900

    次の目標地点を送るタイミングをマニュアルに

--- ar/src/robot_plan/src/motion.cpp
+++ ar/src/robot_plan/src/motion.cpp
@@ -29,15 +29,18 @@ public:
     goal_point_list.push(dummy_goal);
   }
 
-  void checkReachGoal(const std_msgs::Bool &msg) {
+  bool is_reach_goal;
+  void checkReachGoal(const std_msgs::Bool &msg) { is_reach_goal = msg.data; }
+  void sendNextGoal() {
     using std::queue;
-    if (msg.data && !goal_point_list.empty()) {
+    if (!goal_point_list.empty()) {
       goal_point = goal_point_list.front();
       ROS_INFO_STREAM("Next Goal Point is " << goal_point.x << ", "
                                             << goal_point.y << ", "
                                             << goal_point.theta * 180 / M_PI);
       goal_point_list.pop();
       goal_point_pub.publish(goal_point);
+      is_reach_goal = false;
     }
   }
 
@@ -56,6 +59,7 @@ int main(int argc, char **argv) {
   constexpr double FREQ = 300;
   ros::Rate loop_rate(FREQ);
   planner.addGoal(1000, 1000, 0);
+  planner.sendNextGoal();
 
   while (ros::ok()) {
     ros::spinOnce();
