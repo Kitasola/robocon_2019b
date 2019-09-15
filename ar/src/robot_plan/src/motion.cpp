6716e300b9289e9973d6a4b5dfa2be61b120e565
Kitasola <e1611@s.akashi.ac.jp>
2019-09-16 01:27:24 +0900

    座標&行動管理実装

--- ar/src/robot_plan/src/motion.cpp
+++ ar/src/robot_plan/src/motion.cpp
@@ -36,7 +36,6 @@ public:
   }
 
 private:
-  std::queue<geometry_msgs::Pose2D> goal_point_list;
   ros::Subscriber reach_goal_sub;
   ros::Publisher goal_point_pub;
   geometry_msgs::Pose2D goal_point = {};
@@ -48,18 +47,60 @@ using Pi = Pigpiod;
 struct GoalInfo {
   int x;
   int y;
-  int velocity_x;
-  int velocity_y;
   int yaw; // degree あとでradに変換する
   int action_type; // 0: 通過, 1: 2段目昇降, 2: ハンガー, 3: バスタオル, 4:
                    // 3段目昇降, 5: シーツ
-  int action_value // 必要なら使う e.g. 高さ
+  int action_value; // 必要なら使う e.g. 高さ
+  int velocity_x;
+  int velocity_y;
 };
 
-class GoalManager
+class GoalManager {
+public:
+  void add(GoalInfo goal) { map_.push_back(goal); }
+  void add(int x, int y, int yaw, int action_type = 0, int action_value = 0,
+           int velocity_x = 0, int velocity_y = 0) {
+    GoalInfo dummy_goal;
+    dummy_goal.x = x;
+    dummy_goal.y = y;
+    dummy_goal.yaw = yaw;
+    dummy_goal.action_type = action_type;
+    dummy_goal.action_value = action_value;
+    dummy_goal.velocity_x = velocity_x;
+    dummy_goal.velocity_y = velocity_y;
+    map_.push_back(dummy_goal);
+  }
 
-    int
-    main(int argc, char **argv) {
+  void next() {
+    if (map_id_ < map_.size()) {
+      ++map_id_;
+      now = map_.at(map_id_);
+    }
+  }
+
+  void restart() {
+    map_id_ = -1;
+    next();
+  };
+
+  void reset() { map_.resize(0); }
+
+  geometry_msgs::Pose2D getPtp() {
+    geometry_msgs::Pose2D dummy_goal;
+    dummy_goal.x = now.x;
+    dummy_goal.y = now.y;
+    dummy_goal.theta = now.yaw / 180.0 * M_PI;
+    return dummy_goal;
+  }
+
+  GoalInfo now;
+
+private:
+  std::vector<GoalInfo> map_;
+  int map_id_;
+};
+
+int main(int argc, char **argv) {
   ros::init(argc, argv, "motion_planner");
   ros::NodeHandle n;
   Ptp planner(&n, "wheel");
@@ -68,19 +109,38 @@ class GoalManager
   ros::Rate loop_rate(FREQ);
 
   // 座標追加
-  std::vector goal_map<GoalInfo>;
-  int goal_map_id = 0;
-  goal_map.push_buck();
+  GoalManager goal_map;
+  goal_map.add(5400, 5500, 0);
+  goal_map.add(3600, 5500, 0);
+  goal_map.add(3600, 5250, 0);
+  goal_map.add(3600, 5500, 0);
+  goal_map.add(5400, 5500, 0);
 
-  /* constexpr int START_PIN = 18; */
-  /* Pi::gpio().set(18, IN, PULL_DOWN); */
+  constexpr int START_PIN = 18;
+  Pi::gpio().set(18, IN, PULL_DOWN);
 
-  /* while (!Pi::gpio().read(START_PIN)) { */
-  /*   loop_rate.sleep(); */
-  /* }; */
+  while (!Pi::gpio().read(START_PIN)) {
+    loop_rate.sleep();
+  };
+  planner.sendNextGoal(goal_map.getPtp());
 
   while (ros::ok()) {
     ros::spinOnce();
+
+    bool is_send_next_goal = false;
+    if (planner.is_reach_goal) {
+      switch (goal_map.now.action_type) {
+      case 0:
+        is_send_next_goal = true;
+        break;
+      }
+    }
+
+    if (is_send_next_goal) {
+      goal_map.next();
+      planner.sendNextGoal(goal_map.getPtp());
+    }
+
     loop_rate.sleep();
   }
 }
