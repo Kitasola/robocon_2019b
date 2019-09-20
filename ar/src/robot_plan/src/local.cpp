473289921e12d3b7dd912b80735987fd33b55ce4
Kitasola <e1611@s.akashi.ac.jp>
2019-09-20 12:21:08 +0900

    なんかSおかしい

--- ar/src/robot_plan/src/local.cpp
+++ ar/src/robot_plan/src/local.cpp
@@ -244,10 +244,10 @@ private:
   ros::Publisher velocity_pub, reach_goal_pub;
   geometry_msgs::Twist send_twist, goal_velocity;
   double velocity_final_prev[2] = {};
-  constexpr static double VELOCITY_MIN = 600, VELOCITY_MAX = 3000,
-                          ACCEL_MAX = 2500;
+  constexpr static double VELOCITY_MIN = 300, VELOCITY_MAX = 3000,
+                          ACCEL_MAX = 1500;
   constexpr static double ERROR_DISTANCE_MAX = 40;
-  constexpr static double ROOT_FOLLOW = 15;
+  constexpr static double ROOT_FOLLOW = 0;
   std::vector<AccelMap> velocity_map[2];
   constexpr static int MAP_SEARCH_RANGE = 5;
   int map_id[2] = {};
