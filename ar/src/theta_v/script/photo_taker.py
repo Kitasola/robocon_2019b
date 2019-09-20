225efc4c6ca8d59b3205012565938ad48b5b98f8
Kitasola <e1611@s.akashi.ac.jp>
2019-09-21 01:56:16 +0900

    保存もできた

--- ar/src/theta_v/script/photo_taker.py
+++ ar/src/theta_v/script/photo_taker.py
@@ -8,7 +8,7 @@ camera_uri = 'http://192.168.1.1:80/'
 # Check Battery
 state = json.loads(requests.post(camera_uri + 'osc/state').text)
 battery = state["state"]["batteryLevel"]
-print("RICOH THETA V Power: " + str(battery * 100) + "%")
+print "RICOH THETA V Power: " + str(battery * 100) + "%"
 
 # Take Picture
 command = {"name": "camera.takePicture"}
@@ -17,16 +17,23 @@ result = requests.post(camera_uri + 'osc/commands/execute', data=json.dumps(comm
 # Wait Saving
 command = {"stateFingerprint": "FIG_0000"}
 prev_finger = json.loads(requests.post(camera_uri + 'osc/checkForUpdates', data=json.dumps(command), headers={'Content-Type': 'application/json'}).text)["stateFingerprint"]
-print(prev_finger)
+print prev_finger
 
 command = {"stateFingerprint": prev_finger}
 now_finger = prev_finger
 while now_finger == prev_finger:
-  print("Wait ...")
+  print "Wait ..."
   time.sleep(1)
   now_finger = json.loads(requests.post(camera_uri + 'osc/checkForUpdates', data=json.dumps(command), headers={'Content-Type': 'application/json'}).text)["stateFingerprint"]
 
 # Save
 state = json.loads(requests.post(camera_uri + 'osc/state').text)
 file_uri = state["state"]["_latestFileUrl"]
-print(file_uri)
+save_file = requests.get(file_uri)
+try:
+  with open(save_file, "wb") as out:
+    out.write(picture.content)
+    except KeyboardInterrupt:
+      break
+    except Exception as err:
+      print "%s" % (err)
