1db11e742e18b179283d4d2419835b84b3359da3
Kitasola <e1611@s.akashi.ac.jp>
2019-09-21 01:07:22 +0900

    RICOH THETA Vによるリモート撮影成功

---
+++ ar/src/theta_v/script/take_picture.py
@@ -0,0 +1,31 @@
+import time
+import requests
+import json
+
+camera_uri = 'http://192.168.1.1:80/'
+
+# Check Battery
+state = json.loads(requests.post(camera_uri + 'osc/state').text)
+battery = state["state"]["batteryLevel"]
+print("RICOH THETA V Power: " + str(battery * 100) + "%")
+
+# Take Picture
+command = {"name": "camera.takePicture"}
+result = requests.post(camera_uri + 'osc/commands/execute', data=json.dumps(command), headers={'Content-Type': 'application/json'})
+
+# Wait Saving
+command = {"stateFingerprint": "FIG_0000"}
+prev_finger = json.loads(requests.post(camera_uri + 'osc/checkForUpdates', data=json.dumps(command), headers={'Content-Type': 'application/json'}).text)["stateFingerprint"]
+print(prev_finger)
+
+command = {"stateFingerprint": prev_finger}
+now_finger = prev_finger
+while now_finger == prev_finger:
+  print("Wait ...")
+  time.sleep(1)
+  now_finger = json.loads(requests.post(camera_uri + 'osc/checkForUpdates', data=json.dumps(command), headers={'Content-Type': 'application/json'}).text)["stateFingerprint"]
+
+# Save
+state = json.loads(requests.post(camera_uri + 'osc/state').text)
+file_uri = state["state"]["_latestFileUrl"]
+print(file_uri)
