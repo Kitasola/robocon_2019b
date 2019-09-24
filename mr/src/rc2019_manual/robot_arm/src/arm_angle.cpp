5391f09558fab79b5b9106b6b9bbd4182f9b56c3
Kitasola <e1611@s.akashi.ac.jp>
2019-09-24 09:49:28 +0900

    MR移設

--- mr/src/rc2019_manual/robot_arm/src/arm_angle.cpp
+++ mr/src/rc2019_manual/robot_arm/src/arm_angle.cpp
@@ -7,9 +7,13 @@ using std::cout;
 using std::endl;
 
 std_msgs::Float64MultiArray angle;
+std_msgs::Float64MultiArray angle_check;
 
 constexpr int A = 408;
 constexpr int B = 286;
+constexpr double calibration_angle_1 = M_PI / 2;
+constexpr double calibration_angle_2 = M_PI - (M_PI / 6);
+
 float x, y;
 
 void positionCallback(const std_msgs::Float64MultiArray &position);
@@ -19,18 +23,25 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "robot_arm");
 	ros::NodeHandle n;
 	ros::Publisher robot_arm_pub = n.advertise<std_msgs::Float64MultiArray>("arm_angle", 10);
+	ros::Publisher check_pub = n.advertise<std_msgs::Float64MultiArray>("check", 10);
 	ros::Subscriber robot_arm_sub = n.subscribe("angle_info", 10, positionCallback);
 	float angle_1 = 0, angle_2 = 0;
+	angle_check.data.resize(2);
 	angle.data.resize(2);
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(100);
 
 	while(ros::ok()){
 		angle_calcurate(&angle_1, &angle_2);
 		ROS_INFO("angle_1 = %lf", angle_1 * (180 / M_PI));
 		ROS_INFO("angle_2 = %lf", angle_2 * (180 / M_PI));
-		angle.data[0] = (M_PI / 2) - angle_1;
-		angle.data[1] = angle_1 - angle_2;
+		angle_check.data[0] = x;
+		angle_check.data[1] = y;
+		//angle.data[0] = (M_PI / 2) - angle_1;
+		//angle.data[1] = angle_1 - angle_2;
+		angle.data[0] = angle_1 - calibration_angle_1;
+		angle.data[1] = angle_2 - calibration_angle_2;
 		robot_arm_pub.publish(angle);
+		check_pub.publish(angle_check);
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
@@ -47,4 +58,5 @@ void angle_calcurate(float *angle_1, float *angle_2){
 	float theta_1 = atan2(y, x);
 	float theta_2 = acos(-1 * (pow(B, 2) - pow(A, 2) - pow(r, 2)) / (2 * A * r));
 	*angle_1 = (float)theta_1 + theta_2;
+        angle_check.data[0] = *angle_1;
 }
--- mr/src/rc2019_manual/robot_calibration/src/calibration.cpp
+++ mr/src/rc2019_manual/robot_calibration/src/calibration.cpp
@@ -16,19 +16,28 @@ void answer_callback(const std_msgs::String &file_receive){
     }
 }
 
+bool flag_calibration = false;
+
+void joy_callback(const three_omuni::button &button){
+    button.calibration == 1 ? flag_calibration = true : flag_calibration = flag_calibration;
+}
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "robot_calibtarion");
     ros::NodeHandle n;
     ros::Publisher calibration_pub = n.advertise<std_msgs::String>("calibration", 10);
     ros::Subscriber calibration_sub = n.subscribe("calibration_answer", 30, answer_callback);
+    ros::Subscriber controller_sub = n.subscribe("controller_info", 10, joy_callback);
     ros::Rate loop_rate(100);
 
     while(ros::ok()){
-        std_msgs::String file_send;
-        if(robot_status == 0) file_send.data = "gyro";
-        if(robot_status == BIT_FLAG_GYRO) file_send.data = "arm";
-        if(robot_status == ALL_CALIBRATION) file_send.data = "complete";
-	calibration_pub.publish(file_send);
+	if(flag_calibration == true){
+           std_msgs::String file_send;
+           if(robot_status == 0) file_send.data = "gyro";
+           if(robot_status == BIT_FLAG_GYRO) file_send.data = "arm";
+           if(robot_status == ALL_CALIBRATION) file_send.data = "complete";
+	   calibration_pub.publish(file_send);
+	}
         ros::spinOnce();
         loop_rate.sleep();
     }
