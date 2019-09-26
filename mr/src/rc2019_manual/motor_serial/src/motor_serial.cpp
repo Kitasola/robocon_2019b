2acef2c258fe6a6d15c519709b6a9e3a616dd6c0
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-09-26 16:10:45 +0900

    コンパイルしてない

--- mr/src/rc2019_manual/motor_serial/src/motor_serial.cpp
+++ mr/src/rc2019_manual/motor_serial/src/motor_serial.cpp
@@ -17,6 +17,7 @@ int main(int argc, char **argv){
 	ros::ServiceServer service_wheel = n.advertiseService("motor_info", motorSerialSend);
 	ros::ServiceServer service_arm = n.advertiseService("robot_arm", motorSerialSend);
 	ros::ServiceServer service_calibration = n.advertiseService("arm_calibration", motorSerialSend);
+	ros::ServiceServer service_expansion = n.advertiseService("robot_expansion", motorSerialSend);
 	ROS_INFO_STREAM("Start MotorSerial");
 
 	ros::spin();
---
+++ mr/src/rc2019_manual/robot_expansion/src/expansion.cpp
@@ -0,0 +1,46 @@
+#include<ros/ros.h>
+#include<std_msgs/String.h>
+#include"motor_serial/motor_serial.h"
+#include<three_omuni/button.h>
+
+motor_serial::motor_serial srv;
+ros::ServiceClient expansion_client;
+bool controller = false;
+void expansionCallback(const std_msgs::String &msg){
+    if(controller == false){
+        int data_now = 0;
+        if(msg.data == "fase1") data_now = 10;
+        if(msg.data == "fase2") data_now = 15;
+        if(msg.data == "fase3") data_now = 20;
+        srv.request.id = 2;
+        srv.request.cmd = 4;
+        srv.request.data = data_now;
+        expansion_client.call(srv);
+    }
+}
+
+int dummy;
+
+void controller_callback(const three_omuni::button &msg){
+    static int data_expansion = 0;
+    if(msg.expansion_up != false){
+        srv.request.id = 2;
+        srv.request.cmd = 4;
+        srv.request.data = ++data_expansion;
+    }
+    if(msg.expansion_down != false){
+        srv.request.id = 2;
+        srv.request.cmd = 4;
+        srv.request.data = --data_expansion;
+    }
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argc, "expansion");
+    ros::NodeHandle n;
+    ros::Subscriber expansion_sub = n.subscribe("expansion_switch_info", 10, expansionCallback);
+    expansion_client = n.serviceClient<motor_serial::motor_serial>("robot_expansion");
+
+    ros::spin();
+    return 0;
+}
