d12f4fd9b9ed0cca6ac154748afec807ec02e5bb
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-09-27 10:59:17 +0900

    change controller key

--- mr/src/rc2019_commander/src/controller.cpp
+++ mr/src/rc2019_commander/src/controller.cpp
@@ -10,6 +10,12 @@ rc2019_commander::button data; //button message
 
 double stick_x, stick_y, rotation_right_law, rotation_left_law, angle_move, velocity_move, speed;
 
+namespace akashi{
+    int map(double x, double in_min, double in_max, int out_min, int out_max) {
+        return (double)(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+}
+
 void joy_callback(const sensor_msgs::Joy &joy_msg){
     stick_x = -1 * joy_msg.axes[0];
     stick_y = joy_msg.axes[1];
@@ -17,11 +23,34 @@ void joy_callback(const sensor_msgs::Joy &joy_msg){
     speed = hypot(stick_x, stick_y) * 255;//caululation of speed of movement direction of robot
     if(speed > 255) speed = 255;
     data.move_speed = speed;
+    data.move_turn_right = akashi::map(joy_msg.axes[13], 1, -1, 0, 255);
+    data.move_turn_left = akashi::map(joy_msg.axes[12], 1, -1, 0, 255);
     //I have not put the rotation component yet
     data.calibration = joy_msg.buttons[16];
-    data.arm_data = joy_msg.buttons[13];
+    data.arm_data_1 = joy_msg.buttons[5];
+    data.arm_data_2 = joy_msg.buttons[7];
     data.turn_right = joy_msg.buttons[9];
     data.turn_left = joy_msg.buttons[8];
+    data.expansion_up = joy_msg.buttons[4];
+    data.expansion_down = joy_msg.buttons[6];
+    data.speed_half = joy_msg.buttons[11];
+    data.loading = joy_msg.buttons[14];
+    data.shooting = joy_msg.buttons[15];
+    data.hand = joy_msg.buttons[10];
+    data.laundry_case_open = joy_msg.buttons[12];
+    data.laundry_case_close = joy_msg.buttons[13];
+    //robot_arm position key(right key)
+    if(joy_msg.buttons[4] && joy_msg.buttons[10]){
+        data.expansion_up_key = true;
+    }else{
+        data.expansion_up_key = false;
+    }
+    //robot_arm position key(left key)
+    if(joy_msg.buttons[6] && joy_msg.buttons[10]){
+        data.expansion_down_key = true;
+    }else{
+        data.expansion_down_key = false;
+    }
 }
 
 void check_callback(const std_msgs::Bool &check);
