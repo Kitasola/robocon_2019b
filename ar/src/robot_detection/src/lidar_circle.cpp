17806e90f081bf9d7f0ff1d0a9ba5ca067675477
Kitasola <e1611@s.akashi.ac.jp>
2019-09-30 11:08:34 +0900

    rviz用にtopic追加

--- ar/src/robot_detection/src/lidar_circle.cpp
+++ ar/src/robot_detection/src/lidar_circle.cpp
@@ -1,7 +1,9 @@
+#include <geometry_msgs/Point32.h>
 #include <geometry_msgs/Pose2D.h>
 #include <random>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
 #include <std_msgs/String.h>
 #include <string>
 #include <vector>
@@ -13,28 +15,24 @@ void checkGlobalMessage(const std_msgs::String msg) {}
 constexpr double MIN_SCAN_LENGTH = 0.25, MAX_SCAN_LENGTH = 7.695, // m
     SCAN_START_ANGLE = -87.0 / 180 * M_PI,
                  SCAN_FINISH_ANGLE = 87.0 / 180 * M_PI; // rad
-struct Point {
-  int x; // mm
-  int y; // mm
-};
 
 bool can_detection = false;
-std::vector<Point> scan_data; // 相対位置
+sensor_msgs::PointCloud scan_data; // 相対位置
 void getLidarScan(const sensor_msgs::LaserScan msgs) {
   int scan_start_id =
       (SCAN_START_ANGLE - msgs.angle_min) / msgs.angle_increment;
   int scan_finish_id =
       (SCAN_FINISH_ANGLE - msgs.angle_min) / msgs.angle_increment;
-  scan_data.resize(0);
+  scan_data.points.resize(0);
   for (int i = scan_start_id; i < scan_finish_id + 1; ++i) {
     if (msgs.ranges[i] >= MIN_SCAN_LENGTH &&
         msgs.ranges[i] <= MAX_SCAN_LENGTH) {
-      Point dummy_point;
+      geometry_msgs::Point32 dummy_point;
       dummy_point.x = msgs.ranges[i] * 1000 *
                       cos(i * msgs.angle_increment + msgs.angle_min);
       dummy_point.y = msgs.ranges[i] * 1000 *
                       sin(i * msgs.angle_increment + msgs.angle_min);
-      scan_data.push_back(dummy_point);
+      scan_data.points.push_back(dummy_point);
     }
   }
   can_detection = true;
@@ -53,19 +51,19 @@ struct LineParam {
 std::random_device rnd;
 std::mt19937 mt(rnd());
 ModelParam makeModel() {
-  std::uniform_int_distribution<> point_id(0, scan_data.size() - 1);
-  Point sample[3];
+  std::uniform_int_distribution<> point_id(0, scan_data.points.size() - 1);
+  geometry_msgs::Point32 sample[3];
   for (int i = 0; i < 3; ++i) {
-    sample[i] = scan_data.at(point_id(mt));
+    sample[i] = scan_data.points.at(point_id(mt));
     ROS_INFO_STREAM(sample[i].x << ", " << sample[i].y);
   }
 
   LineParam bisector[2];
   for (int i = 0; i < 2; ++i) {
-    bisector[i].a = (double)(sample[i + 1].x - sample[i].x) /
-                    (sample[i + 1].y - sample[i].y);
-    bisector[i].b = (double)(sample[i + 1].y + sample[i].y) / 2 -
-                    bisector[i].a * (double)(sample[i + 1].x + sample[i].x) / 2;
+    bisector[i].a =
+        (sample[i + 1].x - sample[i].x) / (sample[i + 1].y - sample[i].y);
+    bisector[i].b = (sample[i + 1].y + sample[i].y) / 2 -
+                    bisector[i].a * (sample[i + 1].x + sample[i].x) / 2;
   }
 
   ModelParam result;
@@ -75,21 +73,23 @@ ModelParam makeModel() {
   return result;
 }
 
+sensor_msgs::PointCloud match_scan;
 constexpr int MODEL_RADIUS = 720, MAX_ERROR_MODEL = 10, MIN_FIT_POINT = 100,
               MAX_LOOP_COUNT = 200;
 ModelParam checkModel() {
   int fit_point_count = 0;
   for (int i = 0; i < MAX_LOOP_COUNT; ++i) {
     fit_point_count = 0;
+    match_scan.points.resize(0);
     ModelParam model = makeModel();
-    for (point : scan_data) {
+    for (point : scan_data.points) {
       if (abs(pow2((point.x - model.x)) + pow2((point.y - model.y)) -
               MODEL_RADIUS) < MAX_ERROR_MODEL) {
+        match_scan.points.push_back(point);
         ++fit_point_count;
       }
     }
     if (fit_point_count > MIN_FIT_POINT) {
-      ROS_INFO_STREAM(fit_point_count);
       return model;
     }
   }
@@ -99,10 +99,17 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "lidar_detection_circle");
   ros::NodeHandle n;
   ros::Subscriber scan_sub = n.subscribe("scan", 1, getLidarScan);
+  ros::Publisher raw_scan_pub =
+      n.advertise<sensor_msgs::PointCloud>("lidar/raw_scan", 1);
+  ros::Publisher match_scan_pub =
+      n.advertise<sensor_msgs::PointCloud>("lidar/match_scan", 1);
   ros::Publisher robot_pose_pub =
       n.advertise<geometry_msgs::Pose2D>("lidar/robot_pose", 1);
   geometry_msgs::Pose2D robot_pose;
-  Point LIDAR_POSITION = {6250, 5000}; // mm
+  geometry_msgs::Point32 LIDAR_POSITION; // mm
+  LIDAR_POSITION.x = 6250;
+  LIDAR_POSITION.y = 5000;
+  LIDAR_POSITION.z = 0;
 
   constexpr int FREQ = 20;
   ros::Rate loop_rate(FREQ);
@@ -117,6 +124,9 @@ int main(int argc, char **argv) {
       robot_pose.y = robot_model.y;
       ROS_INFO_STREAM(robot_pose.x << ", " << robot_pose.y);
       robot_pose_pub.publish(robot_pose);
+
+      raw_scan_pub.publish(scan_data);
+      match_scan_pub.publish(match_scan);
       can_detection = false;
     }
 
