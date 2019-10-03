1da795ddd4d765379b715f07c02e4a26672e73e1
Kitasola <e1611@s.akashi.ac.jp>
2019-10-04 08:43:23 +0900

    safe書きかえ&スイッチ確認用LED

--- ar/mdd_slave/mdd1/main.cpp
+++ ar/mdd_slave/mdd1/main.cpp
@@ -5,8 +5,6 @@
 ScrpSlave slave(PA_9, PA_10, PA_12, SERIAL_TX, SERIAL_RX, 0x0803e000);
 
 constexpr int NUM_PORT = 5;
-// 0: Motor, 1: Encoder, 2: Other
-constexpr int PORT_FUNCTION[NUM_PORT] = {0, 2, 0, 2, 2};
 
 constexpr int NUM_MOTOR_PORT = 4;
 constexpr int MAX_PWM = 250;
@@ -18,10 +16,8 @@ constexpr PinName MOTOR_PIN[NUM_MOTOR_PORT][3] = {{PB_0, PB_1, PB_3},
                                                   {PB_6, PA_11, PB_7}};
 
 constexpr int NUM_ENCODER_PORT = 4;
-constexpr int RANGE = 200;
 constexpr PinName ENCODER_PIN[NUM_ENCODER_PORT][2] = {
     {PA_0, PA_4}, {PA_1, PA_3}, {PA_8, PA_7}, {PB_6, PA_11}};
-RotaryInc *rotary[NUM_ENCODER_PORT];
 
 float map(float value, float from_low, float from_high, float to_low,
           float to_high) {
@@ -65,15 +61,6 @@ bool spinMotor(int cmd, int rx_data, int &tx_data) {
   return spinMotor(cmd - 2, rx_data);
 }
 
-bool safe(int cmd, int rx_data, int &tx_data) {
-  for (int i = 0; i < 4; ++i) {
-    if (PORT_FUNCTION[i] == 4) {
-      spinMotor(i, 0);
-    }
-  }
-  return true;
-}
-
 int hanger_goal_speed = 0, hanger_current_speed = 0;
 bool serveHanger(int cmd, int rx_data, int &tx_data) {
   hanger_goal_speed = rx_data;
@@ -81,35 +68,36 @@ bool serveHanger(int cmd, int rx_data, int &tx_data) {
   return true;
 }
 
+bool safe(int cmd, int rx_data, int &tx_data) {
+  hanger_goal_speed = 0;
+  return true;
+}
+
 int main() {
-  if (PORT_FUNCTION[0] == 0) {
-    slave.addCMD(2, spinMotor);
-  }
-  if (PORT_FUNCTION[1] == 0) {
-    rotary[0] = new RotaryInc(ENCODER_PIN[0][0], ENCODER_PIN[0][1], RANGE, 1);
-  }
-  for (int i = 2; i < NUM_PORT; ++i) {
-    switch (PORT_FUNCTION[i]) {
-    case 0:
-      slave.addCMD(i + 1, spinMotor);
-      break;
-    case 1:
-      rotary[i - 1] =
-          new RotaryInc(ENCODER_PIN[i - 1][0], ENCODER_PIN[i - 1][1], RANGE, 1);
-      break;
-    }
-  }
+  slave.addCMD(20, serveHanger);
   slave.addCMD(255, safe);
 
-  DigitalIn hanger_limit[2] = {DigitalIn(PA_8), DigitalIn(PB_6)}; // PullUp
-  for (int i = 0; i < 2; ++i) {
-    hanger_limit[i].mode(PullUp);
+  constexpr int NUM_HANGER_SW = 2;
+  constexpr int HANGER_SW_PORT[NUM_HANGER_SW] = {2, 3};
+  DigitalIn hanger_sw[NUM_HANGER_SW] = {
+      DigitalIn(MOTOR_PIN[HANGER_SW_PORT[0]][0]),
+      DigitalIn(MOTOR_PIN[HANGER_SW_PORT[1]][0])}; // PullUp
+  DigitalOut hanger_led[NUM_HANGER_SW]{
+      DigitalOut(MOTOR_PIN[HANGER_SW_PORT[0]][2]),
+      DigitalOut(MOTOR_PIN[HANGER_SW_PORT[1]][2])};
+  for (int i = 0; i < NUM_HANGER_SW; ++i) {
+    hanger_sw[i].mode(PullUp);
+    hanger_led[i] = hanger_sw[i].read();
   }
-  slave.addCMD(20, serveHanger);
+
   while (true) {
     hanger_current_speed = hanger_goal_speed;
-    if ((hanger_current_speed > 0 && hanger_limit[0].read() == 0) ||
-        (hanger_current_speed < 0 && hanger_limit[1].read() == 0)) {
+    int hanger_limit[NUM_HANGER_SW];
+    for (int i = 0; i < NUM_HANGER_SW; ++i) {
+      hanger_led[i] = hanger_limit[i] = hanger_sw[i].read();
+    }
+    if ((hanger_current_speed > 0 && hanger_limit[0] == 0) ||
+        (hanger_current_speed < 0 && hanger_limit[1] == 0)) {
       hanger_current_speed = 0;
     }
     spinMotor(1, -hanger_current_speed);
