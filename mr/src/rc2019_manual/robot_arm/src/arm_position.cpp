0da52565209161cdc3940fb78f7612ac4cd8f364
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-10-03 12:41:43 +0000

    cannot

--- mr/src/rc2019_manual/robot_arm/src/arm_position.cpp
+++ mr/src/rc2019_manual/robot_arm/src/arm_position.cpp
@@ -83,7 +83,7 @@ void controllerButton(const three_omuni::button &button){
     }
 }
 void controllerJoy(const three_omuni::button &button){
-    if(button.arm_data_1 == 0 && button.arm_data_2 == 0){
+    if((button.arm_data_1 == 0 && button.arm_data_2 == 0) && button.speed_half){
         angle_data.data[0] += button.move_arm_x;
         angle_data.data[1] += button.move_arm_y;
     }
