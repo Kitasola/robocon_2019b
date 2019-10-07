127dfae804c4e8ceaeed631712e35e3e77147966
Kitasola <e1611@s.akashi.ac.jp>
2019-10-07 15:16:16 +0900

    腕

--- ar/mdd_slave/mdd3/main.cpp
+++ ar/mdd_slave/mdd3/main.cpp
@@ -1,13 +1,17 @@
 #include <mbed.h>
+#include <pid.hpp>
 #include <rotary_inc.hpp>
 #include <scrp_slave.hpp>
 
 ScrpSlave slave(PA_9, PA_10, PA_12, SERIAL_TX, SERIAL_RX, 0x0803e000);
 
+using namespace arrc;
+
 constexpr int NUM_PORT = 5;
 
 constexpr int NUM_MOTOR_PORT = 4;
 constexpr int MAX_PWM = 250;
+constexpr double MAX_PWM_MBED = 0.95;
 constexpr float PERIOD = 1 / 4000.0;
 constexpr PinName MOTOR_PIN[NUM_MOTOR_PORT][3] = {{PB_0, PB_1, PB_3},
                                                   {PA_1, PA_3, PB_4},
@@ -61,52 +65,74 @@ bool spinMotor(int cmd, int rx_data, int &tx_data) {
   return spinMotor(cmd - 2, rx_data);
 }
 
-DigitalOut arm_solenoid[2] = {DigitalOut(PB_6), DigitalOut(PA_11)};
-DigitalOut arm_led[2] = {DigitalOut(PB_3), DigitalOut(PB_4)};
-bool solenoid(int cmd, int rx_data, int &tx_data) {
-  arm_solenoid[0].write(rx_data % 10);
-  arm_led[0].write(rx_data % 10);
-  arm_solenoid[1].write((rx_data / 10) % 10);
-  arm_led[1].write((rx_data / 10) % 10);
+constexpr int NUM_SOLENOIDS = 2;
+void actSolenoid(int port, int level) {
+  DigitalOut solenoid[NUM_SOLENOIDS] = {DigitalOut(MOTOR_PIN[port][0]),
+                                        DigitalOut(MOTOR_PIN[port][1])};
+  DigitalOut led(MOTOR_PIN[port][2]);
+  for (int i = 0; i < NUM_SOLENOIDS; ++i) {
+    solenoid[i] = level;
+  }
+  led = level;
+}
+
+bool actSolenoid(int cmd, int rx_data, int &tx_data) {
+  actSolenoid(cmd - 40, rx_data);
   return true;
 }
 
-int three_hight_current[2] = {};
-bool checkthreeRegister(int cmd, int rx_data, int &tx_data) {
+int goal_arm_hight = 0;          // [mm]
+int dummy_current_arm_hight = 0; // [mm]
+bool setArmHigh(int cmd, int rx_data, int &tx_data) {
+  // rx_data, tx_data [cm]
+  goal_arm_hight = rx_data * 10;
+  tx_data = dummy_current_arm_hight / 10;
+  return true;
+}
+
+int arm_hight_current = 0;
+bool checkArmRegister(int cmd, int rx_data, int &tx_data) {
   if (rx_data >= 10) {
-    tx_data = three_hight_current[rx_data - 10] / 10;
+    tx_data = arm_hight_current / 10;
   } else {
-    tx_data = three_hight_current[rx_data] % 10;
+    tx_data = arm_hight_current % 10;
   }
   return true;
 }
 
-int three_stage_speed[2] = {};
-bool checkthreeVelocity(int cmd, int rx_data, int &tx_data) {
-  tx_data = three_stage_speed[rx_data];
+int arm_stage_speed = 0;
+bool checkArmVelocity(int cmd, int rx_data, int &tx_data) {
+  tx_data = arm_stage_speed;
   return true;
 }
 
 bool safe(int cmd, int rx_data, int &tx_data) {
-  for (int i = 0; i < 4; ++i) {
-    spinMotor(i, 0);
-  }
+  actSolenoid(40, 0);
+  actSolenoid(40, 1);
   return true;
 }
 
 int main() {
   slave.addCMD(255, safe);
 
-  slave.addCMD(30, setThreeHigh);
-  slave.addCMD(40, solenoid);
+  slave.addCMD(30, setArmHigh);
+  slave.addCMD(40, actSolenoid);
+  slave.addCMD(41, actSolenoid);
+
+  AnalogIn arm_register(PA_0);
+  constexpr int ARM_STAGE_ID = 0;
+  constexpr float ARM_REGISTER_MULTI = 720 / (210.0 / 255) * 5 / 3.3;
+  constexpr int ARM_STAGE_OFFSET = -1196; // 127};
+  constexpr double ARM_STAGE_DOWN = 0.5;
+  PidPosition arm_motor(5.0, 0, 0, 0);
+
   while (true) {
-    AnalogIn three_register AnalogIn(PB_0);
-    constexpr float THREE_REGISTER_MULTI = 10; // mmへの変換倍率
-    constexpr int THREE_STAGE_OFFSET = 0;
-    int measure_high[NUM_REGISTER] = {};
-    for (int 0; i < NUM_REGISTER; ++i) {
-      measure_high[i] =
-          hight_register[i].read() * REGISTER_MULTI - TOW_STAGE_OFFSET;
+    arm_hight_current =
+        arm_register.read() * ARM_REGISTER_MULTI - ARM_STAGE_OFFSET;
+    arm_stage_speed = arm_motor.control(goal_arm_hight, arm_hight_current);
+    if (goal_arm_hight < arm_hight_current) {
+      arm_stage_speed *= ARM_STAGE_DOWN;
     }
+    spinMotor(ARM_STAGE_ID, arm_stage_speed);
   }
 }
