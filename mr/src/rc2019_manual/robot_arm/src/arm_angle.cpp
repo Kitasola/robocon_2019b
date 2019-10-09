fe695a15127b2dbe47d176b2d37a737acc108f18
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-10-09 15:56:10 +0000

    change

--- mr/src/rc2019_manual/robot_arm/src/arm_angle.cpp
+++ mr/src/rc2019_manual/robot_arm/src/arm_angle.cpp
@@ -43,10 +43,10 @@ int main(int argc, char **argv){
 		//angle.data[1] = angle_1 - angle_2;
 		//angle_check.data[0] = angle_1 * (180 / M_PI);
 		//angle_check.data[1] = angle_2 * (180 / M_PI);
-		if(angle_1 >= 3.14) angle_1 = 3.14;
-		if(angle_2 >= 3.14) angle_2 = 3.14;
-		ROS_INFO("angle_1 = %f", 3.14);
-		ROS_INFO("angle_2 = %f", 3.14);
+		if(angle_1 >= M_PI) angle_1 = M_PI;
+		if(angle_2 >= M_PI) angle_2 = M_PI;
+		ROS_INFO("angle_1 = %f", angle_1);
+		ROS_INFO("angle_2 = %f", angle_2);
 		angle.data[0] = (angle_1 - calibration_angle_1) * (180 / M_PI);
 		angle.data[1] = (angle_2 - calibration_angle_2) * (180 / M_PI);
 		ROS_INFO("angle_1 = %d", (int)angle.data[0]);
@@ -77,7 +77,13 @@ void angle_calcurate(double *angle_1, double *angle_2){
 	double theta_1 = atan2(y, x);
 	double theta_2 = acos(-1 * (pow(B, 2) - pow(A, 2) - pow(r, 2)) / (2 * A * r));
 	*angle_1 = (double)theta_1 + theta_2;
-        angle_check.data[0] = *angle_1;
-	if(*angle_1 >= M_PI) *angle_1 = M_PI;
-	if(*angle_2 >= M_PI) *angle_2 = M_PI;
+	angle_check.data[0] = *angle_1;
+	if(*angle_1 >= -M_PI && *angle_1 <= M_PI){
+	}else{
+		*angle_1 = M_PI;
+	}
+	if(*angle_2 >= -M_PI && *angle_2 <= M_PI){
+	}else{
+		*angle_2 = M_PI;
+	}
 }
