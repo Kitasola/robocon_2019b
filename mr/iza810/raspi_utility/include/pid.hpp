2eed7e4fd966b039bc05270ac1ca8184a4bcb3e9
Kitasola <e1611@s.akashi.ac.jp>
2019-10-29 10:27:12 +0900

    time.hpp対応, 出力リミット

--- mr/iza810/raspi_utility/include/pid.hpp
+++ mr/iza810/raspi_utility/include/pid.hpp
@@ -1,38 +1,50 @@
 #ifndef ARRC_PID_HPP
 #define ARRC_PID_HPP
-#include <ros/ros.h>
+#include "time.hpp"
 
-namespace arrc {
-class PidVelocity {
+namespace arrc_raspi {
+class PidPosition {
 public:
-  PidVelocity(double p, double i, double d) {
-    setParam(p, i, d);
-    current = ros::Time::now();
+  PidPosition(double p, double i, double d, double max) {
+    setParam(p, i, d, max);
+    delta_t_.reset();
   }
-  void setParam(double p, double i, double d) {
+  void setParam(double p, double i, double d, double max) {
     p_gain_ = p;
     i_gain_ = i;
     d_gain_ = d;
+    max_control_ = max;
   }
-  double control(double goal, double out) {
-    prev = current;
-    current = ros::Time::now();
-    double delta_t = current.toSec() - prev.toSec();
+  double control(double goal, double out) { return control(goal - out); }
+  double control(double error) {
+    delta_t_.update();
+    delta_t_.reset();
     prev_error_ = current_error_;
-    current_error_ = goal - out;
-    double p_item = p_gain_ * current_error_;
-    double i_item = i_gain_ * current_error_ * delta_t;
-    double d_item = d_gain_ * (current_error_ - prev_error_) / delta_t;
-    control_ = p_item + i_item + d_item;
+    current_error_ = error;
+    p_item_ = p_gain_ * current_error_;
+    i_item_ += i_gain_ * current_error_ * delta_t_.read();
+    if (i_item_ > max_control_) {
+      i_item_ = max_control_;
+    } else if (i_item_ < -max_control_) {
+      i_item_ = -max_control_;
+    }
+    d_item_ = d_gain_ * (current_error_ - prev_error_) / delta_t_.read();
+    control_ = p_item_ + i_item_ + d_item_;
+    if (control_ > max_control_) {
+      control_ = max_control_;
+    } else if (control_ < -max_control_) {
+      control_ = -max_control_;
+    }
     return control_;
   }
 
 private:
   double p_gain_, i_gain_, d_gain_;
-  ros::Time current, prev;
-  double control_ = 0;
+  double p_item_ = 0, i_item_ = 0, d_item_ = 0;
+  Timer delta_t_;
+  double control_ = 0, max_control_ = 0;
   double current_error_ = 0, prev_error_ = 0;
 };
-} // namespace arrc
+} // namespace arrc_raspi
 
 #endif
